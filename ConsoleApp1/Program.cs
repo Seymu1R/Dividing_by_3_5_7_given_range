ffe0f7e91de8e32363998f57fb0d34d4ceffa46f
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-04-18 16:58:56 +0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please include number:  ");
+            int eded = Convert.ToInt32(Console.ReadLine());
+            int num;
+            int num1;
+            int[] arr = new int[1000];
+            int i = 0;
+            
+            if (eded < 50)
+
+            {
+                num = eded / 3;
+                num1 = num * 3;
+                
+                while (num1 > 0)
+                {
+                    
+                    arr[i] = num1;
+                    num1 -= 3;
+                    i++;
+                    
+                    
+                }
+
+                for (int j = 0; j < num; j++)
+
+                {
+                    
+                    Console.Write(" " + arr[j] + " ");
+                }
+            }
+
+            else if (eded > 50 && eded < 100)
+            {
+                
+                num = eded / 5;
+                num1 = num * 5;
+                while (num1 > 0)
+                {
+                    
+                    arr[i] = num1;
+                    num1 -= 5;
+                    i++;
+                    
+                }
+
+                for (int j = 0; j < num; j++)
+                {
+                    
+                    Console.Write(" " + arr[j] + " ");
+                }
+
+            }
+
+            else if (eded > 100)
+            {
+                num = eded / 8;
+                num1 = num * 8;
+                
+                while (num1 > 0)
+                {
+                    
+                    arr[i] = num1;
+                    num1 -= 8;
+                    i++;
+                    
+                }
+
+                for(int j = 0; j < num; j++)
+                {
+                    
+                    Console.Write(" " + arr[j] + " ");
+                }
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("Please, include posetive or int type number");
+            }
+        }
+    }
+}
